e7e4b38e1851ef67a27b0c95f0ebcf04e7f321b9
sethisamarth <sethisamarth1@gmail.com>
2021-09-25 18:27:12 +0530

    [samarth]added:EmpAttendence feature

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,3 +1,15 @@
 #!/bin/bash-x
 
 echo "Welcome to the employ wage problem"
+#variable
+empCheck=$((RANDOM%2))
+
+#contant variable 
+IS_PRESENT=1
+
+if [ $empcheck -eq $IS_PRESENT ]
+then
+     echo "emp is present "
+else
+     echo " emp is absent "
+fi
